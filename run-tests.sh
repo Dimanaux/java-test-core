a2fe476b9d8b0781c4e4078b16e768feee134d5c
Dmitry <barskoidim@gmail.com>
2019-03-23 16:26:55 +0300

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 mkdir -p out
-CLASSPATH=".:lib/*:out"
+CLASSPATH=".:lib/*:out:$CLASSPATH"
 javac -cp $CLASSPATH -d out main/*
 java -cp $CLASSPATH org.junit.runner.JUnitCore main.SolutionTest
